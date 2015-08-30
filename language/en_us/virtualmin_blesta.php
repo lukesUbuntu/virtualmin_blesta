e081581163358e4c6a3885e844b889f28499510a
lukesubuntu <luke@hardiman.co.nz>
2015-08-30 13:26:55 +1200

    skeleton push
    skeleton push

---
+++ language/en_us/virtualmin_blesta.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Language for VirtualMin Blesta Module
+ */
+//module
+$lang['virtualmin.name'] = "Virtualmin ";
+
+//module management
+$lang['virtualmin.module_row'] = "Virtualmin Server";
+$lang['virtualmin.module_row_plural'] = "Server";
+
+?>
