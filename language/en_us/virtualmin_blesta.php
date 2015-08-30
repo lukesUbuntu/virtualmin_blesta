9f603d107ab9ca2da152eadc38c65afffc6a127e
lukesubuntu <luke@hardiman.co.nz>
2015-08-30 15:38:24 +1200

    added manageModule
    added manageModule started language file

--- language/en_us/virtualmin_blesta.php
+++ language/en_us/virtualmin_blesta.php
@@ -8,5 +8,18 @@ $lang['virtualmin.name'] = "Virtualmin ";
 //module management
 $lang['virtualmin.module_row'] = "Virtualmin Server";
 $lang['virtualmin.module_row_plural'] = "Server";
+$lang['virtualmin.add_module_row'] = "Add Server";
+$lang['virtualmin.add_module_group'] = "Add Server Group";
+$lang['virtualmin.manage.module_rows_title'] = "Servers";
+$lang['virtualmin.manage.module_groups_title'] = "Server Groups";
+$lang['virtualmin.manage.module_rows_heading.name'] = "Server Name";
+$lang['virtualmin.manage.module_rows_heading.host_name'] = "Host Name";
+$lang['virtualmin.manage.module_rows_heading.accounts'] = "Accounts";
+$lang['virtualmin.manage.module_rows_heading.options'] = "Options";
+$lang['virtualmin.manage.module_groups_heading.name'] = "Group Name";
+$lang['virtualmin.manage.module_groups_heading.servers'] = "Server Count";
+$lang['virtualmin.manage.module_groups_heading.options'] = "Options";
+$lang['virtualmin.manage.servers.no_results'] = "There are no servers.";
+$lang['virtualmin.manage.servers.groups.no_results'] = "There are no server groups.";
 
 ?>
